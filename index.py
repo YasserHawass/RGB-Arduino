5f0e75fefc968e333c7bd26e08ba0fd12899852d
Yasser Hawass <yasser.hawass@gmail.com>
2019-10-06 01:17:17 +0200

    before a refactor

--- index.py
+++ index.py
@@ -1,14 +1,23 @@
 import serial
 import time
+
 import tkinter as tk
+from tkinter import *
 
 from tks.colors import ColorEntry, ColorDialog, ColorVar
 from tks.color_palette import PaletteSelector
 from tks.color_wheel import ColorWheel
 
+from tkinter import colorchooser
+from tkcolorpicker import askcolor
+
+
 window = tk.Tk()
+redIntensity = IntVar()
+greenIntensity = IntVar()
+blueIntensity = IntVar()
 window.configure(background="gray")
-window.geometry("830x880")
+window.geometry("830x450")
 window.title("Alarm GG")
 
 
@@ -17,6 +26,18 @@ print(arduino.name)         # check which port was really used
 
 def alarm():
     print("Alarm Ready\n")
+    
+    #reference
+    # def updateLights(*_):
+    #     #callback function for sliders
+    #     arduino.write(('{!r}.{!r}.{!r}'.format(int(redIntensity.get()),int(greenIntensity.get()),int(blueIntensity.get()))).encode('utf-8'))
+    #     # print(int(redIntensity, int(greenIntensity.get()), int(blueIntensity.get())))
+
+    def colorb():
+        hello = colorchooser.askcolor(initialcolor='#ff0000')
+        arduino.write(('{!r}.{!r}.{!r}'.format(int(hello[0][0]),int(hello[0][1]),int(hello[0][2]))).encode('utf-8'))
+        print((hello[0]))
+
     def rose():
         print("Rose\n")
         arduino.write(b'255.80.80')
@@ -53,17 +74,26 @@ def alarm():
 
     b3 = tk.Button(window, text="wake me", command=wakeMe, bg="gold", fg="gray7", font=("Comic Sans MS", 15))
 
-    b4 = tk.Button(window, text="BYE", command=quit, bg="gold", fg="gray7", font=("Comic Sans MS", 15))
+    b4 = tk.Button(window, text="Color", command=colorb, bg="gold", fg="gray7", font=("Comic Sans MS", 15))
 
-    color_box = ColorDialog(window, "HEY")
+    bx = tk.Button(window, text="BYE", command=quit, bg="gold", fg="gray7", font=("Comic Sans MS", 15))
+
+    # scaleR = Scale(window, variable = redIntensity, command = updateLights,orient = HORIZONTAL, to=255)         #reference
+    # scaleG = Scale(window, variable = greenIntensity, command = updateLights,orient = HORIZONTAL, to=255)         #reference
+    # scaleB = Scale(window, variable = blueIntensity, command = updateLights, orient = HORIZONTAL, to=255)         #reference
+
+    # color_box = ColorDialog(window, "HEY")         #reference
 
     b1.grid(row=1, column=0, padx=5, pady=10)
     b2.grid(row=1, column=1, padx=5, pady=10)
     b3.grid(row=1, column=2, padx=5, pady=10)
-    b4.grid(row=1, column=3, padx=5, pady=10)
+    b4.grid(row=1, column=4, padx=5, pady=10)
+    bx.grid(row=1, column=5, padx=5, pady=10)
+    # scaleR.grid(row=2, column=1, padx=5, pady=10)         #reference
+    # scaleG.grid(row=2, column=2, padx=5, pady=10)         #reference
+    # scaleB.grid(row=2, column=3, padx=5, pady=10)         #reference
 
-
-
+    # print(askcolor((255, 255, 0), window))        #later
 
     window.mainloop()
 
