04588cf339ca7d25857bb2aedbc0b99e9fc81482
Yasser Hawass <yasser.hawass@gmail.com>
2019-10-05 18:02:57 +0200

    serial byte case switch

--- 07_trying_leds.ino
+++ 07_trying_leds.ino
@@ -5,10 +5,23 @@
 #define REDPIN 9
 #define GREENPIN 10
 #define BLUEPIN 11
- 
 #define FADESPEED 5     // make this higher to slow down
- 
+
+
+int colorRGB[3];
+int redPin = 10;
+int greenPin = 9;
+int bluePin = 11;
+
+int delayVal = 50;
+int blnFade = 0;
+int h_int;
+float h;
+int r=0, g=0, b=0;
+
 void setup() {
+  Serial.begin(9600);
+  
   pinMode(REDPIN, OUTPUT);
   pinMode(GREENPIN, OUTPUT);
   pinMode(BLUEPIN, OUTPUT);
@@ -16,36 +29,52 @@ void setup() {
  
  
 void loop() {
-  int r, g, b;
- 
-  // fade from blue to violet
-  for (r = 0; r < 256; r++) { 
-    analogWrite(REDPIN, r);
-    delay(FADESPEED);
-  } 
-  // fade from violet to red
-  for (b = 255; b > 0; b--) { 
-    analogWrite(BLUEPIN, b);
-    delay(FADESPEED);
-  } 
-  // fade from red to yellow
-  for (g = 0; g < 256; g++) { 
-    analogWrite(GREENPIN, g);
-    delay(FADESPEED);
-  } 
-  // fade from yellow to green
-  for (r = 255; r > 0; r--) { 
-    analogWrite(REDPIN, r);
-    delay(FADESPEED);
-  } 
-  // fade from green to teal
-  for (b = 0; b < 256; b++) { 
-    analogWrite(BLUEPIN, b);
-    delay(FADESPEED);
-  } 
-  // fade from teal to blue
-  for (g = 255; g > 0; g--) { 
-    analogWrite(GREENPIN, g);
-    delay(FADESPEED);
-  } 
+//  if (Serial.available()){
+   if(Serial.available() >= 2){
+     switch( byte( Serial.read() )) {
+       case 'r':
+         colorRGB[0] = Serial.read();
+         blnFade = 0;
+         break;
+       case 'g':
+         colorRGB[1] = Serial.read();
+         blnFade = 0;
+         break;   
+       case 'b':
+         colorRGB[2] = Serial.read();
+         blnFade = 0;
+         break;
+       case 'c':
+         Serial.flush();
+         blnFade = 0;
+         break;
+       case 'f':
+         delayVal = Serial.read();
+         Serial.flush();
+  //       colorFade();
+         blnFade = 1;
+    }
+  }
+  analogWrite(redPin, colorRGB[0]); 
+  analogWrite(greenPin, colorRGB[1]);
+  analogWrite(bluePin, colorRGB[2]);
+  delay(20);
+  
+//    int r, g, b;
+//   
+//    // fade from blue to violet
+//    for (r = 0; r < 256; r++) { 
+//      analogWrite(REDPIN, r);
+//      analogWrite(GREENPIN, r);
+//      analogWrite(BLUEPIN, r-20);
+//      delay(FADESPEED);
+//    } 
+//    // fade from violet to red
+//    for (b = 255; b > 0; b--) { 
+//      analogWrite(REDPIN, b);
+//      analogWrite(GREENPIN, b);
+//      analogWrite(BLUEPIN, b);
+//      delay(FADESPEED);
+//    }
+//  }
 }
