df6cd31ea4596ef70981624526b37e680d12b0fe
Yasser Hawass <yasser.hawass@gmail.com>
2019-10-06 00:03:36 +0200

    new arduino cod

--- 07_trying_leds.ino
+++ 07_trying_leds.ino
@@ -1,80 +1,86 @@
-// color swirl! connect an RGB LED to the PWM pins as indicated
-// in the #defines
-// public domain, enjoy!
- 
-#define REDPIN 9
-#define GREENPIN 10
-#define BLUEPIN 11
-#define FADESPEED 5     // make this higher to slow down
+#include "cie1931.h"
 
+//Uncomment your led type
+#define commonAnode
+//#define commonCathone
 
-int colorRGB[3];
-int redPin = 10;
-int greenPin = 9;
-int bluePin = 11;
+//led ports
+#define ledRed   10
+#define ledGreen 9
+#define ledBlue  11
 
-int delayVal = 50;
-int blnFade = 0;
-int h_int;
-float h;
-int r=0, g=0, b=0;
+//max color led values
+#define maxRed    255
+#define maxGreen  255
+#define maxBlue   255
+
+int red, green, blue = 0;
 
 void setup() {
   Serial.begin(9600);
-  
-  pinMode(REDPIN, OUTPUT);
-  pinMode(GREENPIN, OUTPUT);
-  pinMode(BLUEPIN, OUTPUT);
+  Serial.setTimeout(10);
+
+  pinMode(red, OUTPUT);
+  pinMode(green, OUTPUT);
+  pinMode(blue, OUTPUT);
 }
- 
- 
+
 void loop() {
-//  if (Serial.available()){
-   if(Serial.available() >= 2){
-     switch( byte( Serial.read() )) {
-       case 'r':
-         colorRGB[0] = Serial.read();
-         blnFade = 0;
-         break;
-       case 'g':
-         colorRGB[1] = Serial.read();
-         blnFade = 0;
-         break;   
-       case 'b':
-         colorRGB[2] = Serial.read();
-         blnFade = 0;
-         break;
-       case 'c':
-         Serial.flush();
-         blnFade = 0;
-         break;
-       case 'f':
-         delayVal = Serial.read();
-         Serial.flush();
-  //       colorFade();
-         blnFade = 1;
+  if (Serial.available()){
+    //read int values from serial
+    red = Serial.parseInt();
+    green = Serial.parseInt();
+    blue = Serial.parseInt();
+
+
+
+    //convert rgb to valid values (0-255)
+    if (red > 255){
+      red = 255;
     }
+    else if (red < 0){
+      red = 0;
+    }
+
+    if (green > 255){
+      green = 255;
+    }
+    else if (green < 0){
+      green = 0;
+    }
+
+    if (blue > 255){
+      blue = 255;
+    }
+    else if (blue < 0){
+      blue = 0;
+    }
+
+    Serial.println("Original values: " + String(red) + " " + String(green) +
+    " " + String(blue));
+
+    //remap color values to improve color precision
+    red = map(red, 0, 255, 0, maxRed);
+    green = map(green, 0, 255, 0, maxGreen);
+    blue = map(blue, 0, 255, 0, maxBlue);
+
+    //PWM
+    //common anode
+    #ifdef commonAnode
+      analogWrite(ledRed, cie[red]);
+      analogWrite(ledGreen, cie[green]);
+      analogWrite(ledBlue, cie[blue]);
+    #endif
+
+    //common cathode
+    #ifdef commonCathone
+      analogWrite(ledRed, cie[255 - red]);
+      analogWrite(ledGreen, cie[255 - green]);
+      analogWrite(ledBlue, cie[255 - blue]);
+    #endif
+
+    //print rgb values
+    Serial.println("remaped values: " + String(red) + " " + String(green) +
+    " " + String(blue));
   }
-  analogWrite(redPin, colorRGB[0]); 
-  analogWrite(greenPin, colorRGB[1]);
-  analogWrite(bluePin, colorRGB[2]);
-  delay(20);
-  
-//    int r, g, b;
-//   
-//    // fade from blue to violet
-//    for (r = 0; r < 256; r++) { 
-//      analogWrite(REDPIN, r);
-//      analogWrite(GREENPIN, r);
-//      analogWrite(BLUEPIN, r-20);
-//      delay(FADESPEED);
-//    } 
-//    // fade from violet to red
-//    for (b = 255; b > 0; b--) { 
-//      analogWrite(REDPIN, b);
-//      analogWrite(GREENPIN, b);
-//      analogWrite(BLUEPIN, b);
-//      delay(FADESPEED);
-//    }
-//  }
 }
